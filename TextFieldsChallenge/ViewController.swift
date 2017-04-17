2c8c2a3f2c8198852d0fc1673e23172f5aa42dd3
Victor Emanuel Del Castillo <emanuel.delcastillo@gmail.com>
2017-04-17 10:07:05 -0400

    Hides keyboard when hitting return key in the Lockable Text Field

--- TextFieldsChallenge/ViewController.swift
+++ TextFieldsChallenge/ViewController.swift
@@ -8,29 +8,20 @@
 
 import UIKit
 
-// MARK: ViewController : UIViewController, UITextFieldDelegate -
+// MARK: ViewController : UIViewController, UITextFieldDelegate
 class ViewController: UIViewController, UITextFieldDelegate {
 
-  // MARK: - Outlets -
+  // MARK: - Outlets
   @IBOutlet weak var zipCodeTextField:  UITextField!
   @IBOutlet weak var cashTextField:     UITextField!
   @IBOutlet weak var lockableTextField: UITextField!
   @IBOutlet weak var lockableSwitch:    UISwitch!
   
-  // MARK: - Text Field Properties
-  var enableTextFieldEditing : Bool {
-    if lockableSwitch.isOn {
-      return true
-    }
-    
-    return false
-  }
-  
-  // MARK: - Text Field Delegate Objects -
+  // MARK: - Text Field Delegate Objects
   let zipCodeTextFieldDelegate = ZipCodeTextFieldDelegate()
   let cashTextFieldDelegate    = CashTextFieldDelegate()
   
-  // MARK: - Life Cycle -
+  // MARK: - Life Cycle
   override func viewDidLoad() {
     super.viewDidLoad()
     
@@ -39,12 +30,21 @@ class ViewController: UIViewController, UITextFieldDelegate {
     lockableTextField.delegate = self
   }
   
-  // MARK: - Text Field Delegate Methods -
+  // MARK: - Text Field Delegate Methods
   func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
-    return enableTextFieldEditing
+    return lockableSwitch.isOn
   }
   
-  func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-    return enableTextFieldEditing
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    textField.resignFirstResponder()
+    
+    return true
+  }
+  
+  // MARK: - Action Methods
+  @IBAction func toggleTextField(_ sender: UISwitch) {
+    if !sender.isOn {
+      lockableTextField.resignFirstResponder()
+    }
   }
 }
